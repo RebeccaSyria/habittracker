3ccf8e318fbe4d776fb78dff522754a41011b4ab
Rebecca <rebeccaesyria@gmail.com>
2020-04-13 01:32:23 -0400

    database events for days

--- lib/model/day/day.dart
+++ lib/model/day/day.dart
@@ -12,11 +12,35 @@ class Day {
   Map<Habit, int> habits;
 
   Day({
+    this.id,
     @required this.year,
     @required this.month,
     @required this.date,
-    @required this.weekday
+    @required this.weekday,
+    this.habits
   })  {
     this.id = int.parse(year.toString() + month.toString() + date.toString());
+  }
+
+  Map<String,dynamic> toMap() {
+    return {
+      'id': id,
+      'year': year,
+      'month': month,
+      'date': date,
+      'weekday': weekday,
+      'habits': habits
+    };
+  }
+
+  static Day fromMap(Map<String, dynamic> map) {
+    return Day(
+      id: map['id'],
+      year: map['year'],
+      month: map['month'],
+      date: map['date'],
+      weekday: map['weekday'],
+      habits: map['habits']
+    );
   }
 }
--- lib/model/day/day_dao.dart
+++ lib/model/day/day_dao.dart
@@ -9,5 +9,26 @@ class DayDao {
 
   Future<Database> get _db async => await AppDb.instance.database;
 
+  Future insert(Day day) async {
+    await _dayStore.add(await _db, day.toMap());
+  }
+
+  Future update(Day day) async {
+    final finder = Finder(filter: Filter.byKey(day.id));
+    await _dayStore.update(await _db, day.toMap(), finder: finder);
+  }
+
+  Future getAllSortedByDate() async {
+    final finder = Finder(sortOrders: [
+      SortOrder('year'),
+      SortOrder('month'),
+      SortOrder('date')
+    ]);
+    final recordSnapshots = await _dayStore.find(await _db, finder: finder);
+    return recordSnapshots.map((snapshot) {
+      final day = Day.fromMap(snapshot.value);
+      return day;
+    }).toList();
+  }
 
 }
